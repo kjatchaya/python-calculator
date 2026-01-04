05b1d81c7aa385a402e45c6a2faa7f7dc39e48b4
K J Atchaya <kjatchaya910@gmail.com>
2026-01-04 10:11:53 +0530

    converted calculator to loop-based program

--- calculator.py
+++ calculator.py
@@ -9,26 +9,34 @@ def divide(a,b):
         return "Error: Zero Division"
     else:
         return a/b
+while True:
+    print("Simple Calculator")
+    print("1.add")
+    print("2.subtract")
+    print("3.multiply")
+    print("4.divide")
+    print("5.exit")
 
-print("Simple Calculator")
-print("1.add")
-print("2.subtract")
-print("3.multiply")
-print("4.divide")
+    choice = input("Choose an operation (1/2/3/4/5): ")
 
-choice = input("Choose an operation (1/2/3/4): ")
+    if choice=="5":
+        print("Goodbye")
+        break
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-if choice == "1":
-    print("Result:", add(num1, num2))
-elif choice == "2":
-    print("Result:", subtract(num1, num2))
-elif choice == "3":
-    print("Result:", multiply(num1, num2))
-elif choice == "4":
-    print("Result:", divide(num1, num2))
-else:
-    print("Invalid choice")
+    if choice == "1":
+        print("Result:", add(num1, num2))
+    elif choice == "2":
+        print("Result:", subtract(num1, num2))
+    elif choice == "3":
+        print("Result:", multiply(num1, num2))
+    elif choice == "4":
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid choice")
+
+    
+
 
